4fa714690533e7e0245ee23e02f5c580cbcf1c49
surendra-koritala-tfs <116296837+surendra-koritala-tfs@users.noreply.github.com>
2025-06-06 20:31:47 +0000

    added plugins to call by the sk agent

--- api/plugins/account_owner_plugin.py
+++ api/plugins/account_owner_plugin.py
@@ -13,4 +13,91 @@ class AccountOwnerPlugin(BaseVectorSearchPlugin):
     )
     def account_owner_search(self, query: str, k: int = 3) -> str:
         """Account owner search the Azure AI Search index for relevant information."""
-        return self.search_index(query, k)
+        account_owner_data="""
+account_name,Owner 
+2SEVENTY BIO,Katherine 
+ABBOTT,Stephanie
+ABBVIE,Stephanie
+ABBVIE BIOTECHNOLOGY LTD,Stephanie
+AMGEN,Katherine 
+ARSENAL BIOSCIENCES,Melissa
+ARUP,Shannen
+ASTELLAS PHARMA,
+ASTRAZENECA,Stephanie
+BAYER,Shannen
+BE BIOPHARMA,Melissa
+BI,Stephanie 
+BIOGEN,Stephanie 
+BIONTECH,Stephanie 
+BLUEROCK THERAPEUTICS,Shannen
+BMS,Katherine 
+BRISTOL MYERS SQUIBB CO,Katherine 
+CATALENT,Stephanie
+CATALENT ANAGNI SRL,Stephanie
+CATALENT GOSSELIES PS SA,Stephanie
+CATALENT INC,Stephanie
+CDC,Katherine 
+CEDARS SINAI,Melissa
+CHARLES RIVER LABS,Shannen
+CORNELL UNIV,
+CORTEVA,Melissa
+CSL LTD,Shannen
+CUREVAC,Shannen
+DECIBEL THERAPEUTICS,Katherine
+DIASORIN,Katherine
+ELI LILLY,Shannen
+EUROFINS,Stephanie
+FUJIFILM,Stephanie 
+GARUDA THERAPEUTICS,Katherine
+GILEAD KITE,
+GILEAD/KITE,Katherine 
+GSK,Stephanie 
+HARVARD UNIV,Melissa
+HEALTHTRACKRX,Melissa
+J&J,Shannen
+JOHNS HOPKINS UNIV,Melissa
+JOHNSON & JOHNSON SERVICES INC,Shannen
+KAISER PERMANENTE,Melissa
+KRYSTAL BIOTECH,Katherine
+LAB CORP,Shannen
+LAB CORP OF AMERICA,Shannen
+LARONDE,Katherine
+LONZA,Katherine
+MAKO MEDICAL LABS,Melissa
+MAYO CLINIC,Shannen
+MEMORIAL SLOAN KETTERING,Katherine
+MERCK,Stephanie
+MERCK KGAA,Stephanie
+MODERNA,Shannen
+NATIONAL RESILIENCE,Stephanie
+NOVARTIS,Stephanie
+NOVO NORDISK,Katherine
+OPKO HEALTH,Melissa
+PFIZER,Stephanie
+QUEST DIAGNOSTICS,Shannen
+REGENERON,Katherine 
+REPLIGEN,Shannen
+RING THERAPEUTICS,Melissa
+ROCHE,Katherine 
+SAIL BIOMEDICINES,Katherine
+SANA BIOTECHNOLOGY,Melissa
+SANOFI,Katherine 
+SAREPTA THERAPEUTICS,Katherine
+SATELLITE BIO,Melissa
+ST JUDE,Shannen
+STANFORD UNIV,
+TAKEDA PHARMACEUTICALS,Shannen
+TESSERA THERAPEUTICS,Katherine 
+TOME BIOSCIENCES,Shannen
+UNIV OF CALIFORNIA SYSTEM,Melissa
+UNIV OF NORTH CAROLINA SYSTEM,
+UNIV OF PENNSYLVANIA,Melissa
+VERTEX PHARMACEUTICALS,Stephanie
+VERVE THERAPEUTICS,Melissa
+WASHINGTON UNIV IN ST LOUIS,Melissa
+WUXI,Katherine 
+ZOETIS,Stephanie
+"""
+        # return self.search_index(query, k)
+        return account_owner_data
+    
--- api/plugins/threshold_plugin.py
+++ api/plugins/threshold_plugin.py
@@ -13,4 +13,18 @@ class ThresholdPlugin(BaseVectorSearchPlugin):
     )
     def threshold_search(self, query: str, k: int = 3) -> str:
         """Vector search the Azure AI Search index for relevant information."""
-        return self.search_index(query, k)
+        threshhold_csv_data="""
+as_of_date,order_created_year_quarter,primary_group,product_division_code,business_segment_code,account_name,nba_hier_level,nba_name,nba_child,threshold_value,threshold_deviation,nba_child_metrics_value,relative_benchmark_value,unit_of_measure,recommendation_type,ord_grp,company_region,company_sub_region
+2025-02-01 13:48:43,Q1-2025,Life Sciences,-,-,ARSENAL BIOSCIENCES,primary_group,Improve Order Velocity,% of Orders on hold,0.1500,0.0038,0.1538,,%,Absolute,1,NA,-
+2025-02-01 13:48:43,Q1-2025,Life Sciences,-,-,ARUP,primary_group,Improve Order Velocity,% of Orders on hold,0.1500,0.2406,0.3906,,%,Absolute,1,NA,-
+2025-02-01 13:48:43,Q1-2025,Life Sciences,-,-,BIOGEN,primary_group,Improve Order Velocity,Average hold duration,10.0000,1.5600,11.5600,,days,Absolute,1,NA,-
+2025-02-01 13:48:43,Q1-2025,Life Sciences,-,-,BMS,primary_group,Improve Order Velocity,% of Orders on hold,0.1500,0.3467,0.4967,,%,Absolute,1,NA,-
+2025-02-01 13:48:43,Q1-2025,Life Sciences,-,-,CATALENT,primary_group,Improve Order Velocity,% of Orders on hold,0.1500,0.1912,0.3412,,%,Absolute,1,NA,-
+2025-02-01 13:48:43,Q1-2025,Life Sciences,-,-,CEDARS SINAI,primary_group,Improve Order Velocity,% of Orders on hold,0.1500,0.2506,0.4006,,%,Absolute,1,NA,-
+2025-02-01 13:48:43,Q1-2025,Life Sciences,-,-,CSL LTD,primary_group,Improve Order Velocity,% of Orders on hold,0.1500,0.0328,0.1828,,%,Absolute,1,NA,-
+2025-02-01 13:48:43,Q1-2025,Life Sciences,-,-,FUJIFILM,primary_group,Improve Order Velocity,% of Orders on hold,0.1500,0.0711,0.2211,,%,Absolute,1,NA,-
+2025-02-01 13:48:43,Q1-2025,Life Sciences,-,-,HEALTHTRACKRX,primary_group,Improve Order Velocity,% of Orders on hold,0.1500,0.4245,0.5745,,%,Absolute,1,NA,-
+
+"""
+        # return self.search_index(query, k)
+        return threshhold_csv_data
